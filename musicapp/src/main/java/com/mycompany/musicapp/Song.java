f19318dac76964e770db1950b90bb7ed0808d69d
sahil swaraj <sahilswaraj82@gmail.com>
2025-11-15 23:56:49 +0530

    first commit

---
+++ musicapp/src/main/java/com/mycompany/musicapp/Song.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.musicapp;
+
+/**
+ *
+ * @author sahil
+ */
+public class Song {
+    String title;
+    double duration;
+    
+    public Song(String title, double duration){
+        this.title = title;
+        this.duration = duration;
+    }
+    
+    public Song(){
+        
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public double getDuration(){
+        return duration;
+    }
+    
+    toString
+}
