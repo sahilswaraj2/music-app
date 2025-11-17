c4b0b745119fc5ce6bcd21486cc64169814dbc45
sahil swaraj <sahilswaraj82@gmail.com>
2025-11-17 18:28:37 +0530

    Updated files

--- musicapp/src/main/java/com/mycompany/musicapp/Album.java
+++ musicapp/src/main/java/com/mycompany/musicapp/Album.java
@@ -4,10 +4,26 @@
  */
 package com.mycompany.musicapp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author sahil
  */
 public class Album {
+    private String name;
+    private String artist;
+    
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist, ArrayList<Song> songs) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public Album() {
+    }
+    
     
 }
